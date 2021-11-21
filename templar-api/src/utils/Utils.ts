250966721dd25f126cf876abfc6f311ee38b293a
Martin Belko <martbelko@gmail.com>
2021-11-21 23:15:21 +0100

    Changed != to !==

--- templar-api/src/utils/Utils.ts
+++ templar-api/src/utils/Utils.ts
@@ -8,11 +8,11 @@ export enum ValidationError {
 export class Utils {
     public static getTokenFromAuthHeader(authHeader: string): string | null {
         const parts = authHeader.split(' ');
-        if (parts.length != 2) {
+        if (parts.length !== 2) {
             return null;
         }
 
-        if (parts[0] != 'Bearer') {
+        if (parts[0] !== 'Bearer') {
             return null;
         }
 
