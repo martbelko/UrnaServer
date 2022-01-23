c464bbe887163f1d42d0f84f00291851ffe56085
Martin Belko <martbelko@gmail.com>
2022-01-23 17:48:07 +0100

    Removed unused code

--- templar-app/src/App.tsx
+++ templar-app/src/App.tsx
@@ -2,10 +2,6 @@ import React from 'react';
 import { Route, BrowserRouter as Router, Switch, Redirect } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 
-import Register from './components/Register/Register';
-import Login from './components/Login/Login';
-import Update from './components/Update/Update';
-
 function App(): JSX.Element {
     return (
         <RecoilRoot>
@@ -13,15 +9,6 @@ function App(): JSX.Element {
                 <Switch>
                     <Route path="/index">
                         Not found
-                    </Route>
-                    <Route path="/register">
-                        <Register />
-                    </Route>
-                    <Route path="/login">
-                        <Login />
-                    </Route>
-                    <Route path="/update">
-                        <Update />
                     </Route>
                     <Route path="/">
                         <Redirect to="/index" />
